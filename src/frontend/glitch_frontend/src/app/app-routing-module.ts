bb289d8eb31aadcb83dc910acd52a9987de070b0
Letícia Zalasik <leticiazalasik@gmail.com>
2025-09-02 19:57:38 -0300

    Mudanças de estilo na página app e estilo geral do projeto

--- src/frontend/glitch_frontend/src/app/app-routing-module.ts
+++ src/frontend/glitch_frontend/src/app/app-routing-module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from '../app/pages/create-account/create-account';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create-account', component: CreateAccountComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
