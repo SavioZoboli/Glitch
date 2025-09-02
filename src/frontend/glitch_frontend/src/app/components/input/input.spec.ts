df6519d70cb240dfff732b2f6b60534a71c4e0b4
Letícia Zalasik <leticiazalasik@gmail.com>
2025-09-02 19:56:28 -0300

    Criação do componente input

---
+++ src/frontend/glitch_frontend/src/app/components/input/input.spec.ts
@@ -0,0 +1,22 @@
+import { Input } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+describe('Input', () => {
+  let component: Input;
+  let fixture: ComponentFixture<Input>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [Input]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Input);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
