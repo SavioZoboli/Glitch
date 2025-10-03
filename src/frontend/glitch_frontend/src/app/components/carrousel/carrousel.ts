44214fd6ef9c16f20fff63906ccbb141d402e395
Savio Zoboli <savio.zoboli@gmail.com>
2025-10-03 19:22:00 -0300

    feat: esqueleto do carrousel

---
+++ src/frontend/glitch_frontend/src/app/components/carrousel/carrousel.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+type Caroussel = {
+  img:string;
+  href:string|null;
+  position:'atual'|'anterior'|'proximo'|'escondido';
+
+}
+
+@Component({
+  selector: 'app-carrousel',
+  standalone: false,
+  templateUrl: './carrousel.html',
+  styleUrl: './carrousel.scss'
+})
+export class Carrousel {
+
+carrousels:Caroussel[] = [];
+
+constructor(){
+  this.carrousels = [{
+    img:'Imagem 1',
+    href:null,
+    position:'anterior'
+  },{
+    img:'Imagem 2',
+    href:null,
+    position:'atual'
+  },{
+    img:'Imagem 3',
+    href:null,
+    position:'proximo'
+  },]
+}
+
+
+}
