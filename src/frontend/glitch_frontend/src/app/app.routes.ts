2460e77649b6dce9611a0ed246eafd1025c9a75c
anali-vb <169708546+anali-vb@users.noreply.github.com>
2025-10-09 20:36:51 -0300

    feat(dashboard): ajustes de layout, icons e tema no Glitch Frontend

--- src/frontend/glitch_frontend/src/app/app.routes.ts
+++ src/frontend/glitch_frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CreateAccountComponent } from '../app/pages/create-account/create-account';
 import { LandingPageComponent } from './pages/landing-page/landing-page';
+import { DashboardComponent } from './pages/dashboard/dashboard';
 
 // Apenas o array de rotas, sem o @NgModule
 export const routes: Routes = [
   { path:'', component: LandingPageComponent, pathMatch:'full' },
   { path: 'create-account', component: CreateAccountComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
