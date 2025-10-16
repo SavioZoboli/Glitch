7d0286c175d7c9e0e20f43534aece69a9df5422e
Savio Zoboli <savio.zoboli@gmail.com>
2025-10-16 20:38:13 -0300

    feat: criação do componente de perfil com o layout inicial

--- src/frontend/glitch_frontend/src/app/pages/profile/profile.ts
+++ src/frontend/glitch_frontend/src/app/pages/profile/profile.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Navigation } from "../../components/navigation/navigation";
+import { ButtonComponent } from "../../components/button/button";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [Navigation, ButtonComponent],
   templateUrl: './profile.html',
   styleUrl: './profile.scss'
 })
 export class ProfileComponent {
 
+  constructor(private router:Router){
+
+  }
+
+  editProfile(){
+    this.router.navigate(['update-profile'])
+  }
 }
