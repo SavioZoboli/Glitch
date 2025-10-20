26e780378cb497c1833aa762b064ff1a43ff8e7f
anali-vb <169708546+anali-vb@users.noreply.github.com>
2025-10-20 16:29:41 -0300

    feat(players-list): integração com UsuarioService para listar jogadores

--- src/frontend/glitch_frontend/src/app/pages/players-list/players-list.component.ts
+++ src/frontend/glitch_frontend/src/app/pages/players-list/players-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { InputComponent } from '../../components/input/input';
 import { ButtonComponent } from '../../components/button/button';
 import { Footer } from '../../components/footer/footer';
@@ -7,6 +6,7 @@ import { ThemeToggler } from '../../components/theme-toggler/theme-toggler';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Navigation } from '../../components/navigation/navigation';
+import { UsuarioService } from '../../services/usuario-service'; // ✅ import do service
 
 @Component({
   selector: 'app-players-list',
@@ -15,9 +15,28 @@ import { Navigation } from '../../components/navigation/navigation';
   templateUrl: './players-list.component.html',
   styleUrls: ['./players-list.component.scss']
 })
-export class PlayersListComponent {
+export class PlayersListComponent implements OnInit {
+
   jogadores: any[] = [];
   searchTerm: string = '';
+
+  constructor(private usuarioService: UsuarioService) {} 
+
+  ngOnInit(): void {
+    this.buscarUsuarios(); 
+  }
+
+  buscarUsuarios(): void {
+    this.usuarioService.getUsuarios().subscribe({
+      next: (res) => {
+        this.jogadores = res;
+        console.log('Jogadores carregados:', this.jogadores);
+      },
+      error: (err) => {
+        console.error('Erro ao buscar jogadores:', err);
+      }
+    });
+  }
 
   filteredPlayers() {
     return this.jogadores.filter(player =>
