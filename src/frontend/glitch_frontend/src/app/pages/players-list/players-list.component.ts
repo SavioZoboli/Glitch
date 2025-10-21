55dfa17fa5f5f4d67c5389bd06d4a162b128d96c
Savio Zoboli <savio.zoboli@gmail.com>
2025-10-21 19:11:12 -0300

    feat: alterado retorno dos usuários com async pipe

--- src/frontend/glitch_frontend/src/app/pages/players-list/players-list.component.ts
+++ src/frontend/glitch_frontend/src/app/pages/players-list/players-list.component.ts
@@ -3,44 +3,38 @@ import { InputComponent } from '../../components/input/input';
 import { ButtonComponent } from '../../components/button/button';
 import { Footer } from '../../components/footer/footer';
 import { ThemeToggler } from '../../components/theme-toggler/theme-toggler';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Navigation } from '../../components/navigation/navigation';
-import { UsuarioService } from '../../services/usuario-service'; // ✅ import do service
+import { Usuario, UsuarioService } from '../../services/usuario-service'; // ✅ import do service
+import { catchError, EMPTY, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-players-list',
   standalone: true,
-  imports: [InputComponent, ButtonComponent, Footer, ThemeToggler, FormsModule, CommonModule, Navigation],
+  imports: [InputComponent, ButtonComponent, ThemeToggler, FormsModule, CommonModule, Navigation],
   templateUrl: './players-list.component.html',
   styleUrls: ['./players-list.component.scss']
 })
 export class PlayersListComponent implements OnInit {
+// A variável agora armazena o Observable, e não o array.
+  jogadores$!: Observable<Usuario[]>;
+  filtroControl: FormControl = new FormControl('');
 
-  jogadores: any[] = [];
-  searchTerm: string = '';
-
-  constructor(private usuarioService: UsuarioService) {} 
+  constructor(private usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
-    this.buscarUsuarios(); 
+    this.buscarUsuarios();
   }
 
   buscarUsuarios(): void {
-    this.usuarioService.getUsuarios().subscribe({
-      next: (res) => {
-        this.jogadores = res;
-        console.log('Jogadores carregados:', this.jogadores);
-      },
-      error: (err) => {
+    this.jogadores$ = this.usuarioService.getUsuarios().pipe(
+      catchError(err => {
+        // Se ocorrer um erro na API, podemos logar e retornar um array vazio.
+        // Isso evita que a tela quebre se a chamada falhar.
         console.error('Erro ao buscar jogadores:', err);
-      }
-    });
-  }
-
-  filteredPlayers() {
-    return this.jogadores.filter(player =>
-      player.nome.toLowerCase().includes(this.searchTerm.toLowerCase())
+        return EMPTY; // ou return of([]); se preferir emitir um array vazio.
+      })
     );
   }
 }
