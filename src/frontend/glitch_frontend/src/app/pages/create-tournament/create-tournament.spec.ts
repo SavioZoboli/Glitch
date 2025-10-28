101409cd7d724ecda701bb64958158015a92c7c2
Letícia Zalasik <leticiazalasik@gmail.com>
2025-10-27 22:41:17 -0300

    Criação do create-tournament html, css, ts, com validações específicas para cada campo e mensagens de erro

--- src/frontend/glitch_frontend/src/app/pages/create-tournament/create-tournament.spec.ts
+++ src/frontend/glitch_frontend/src/app/pages/create-tournament/create-tournament.spec.ts
@@ -1,50 +1,23 @@
-import { CreateTournament } from './create-tournament';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TournamentList } from '../tournament-list/tournament-list';
 
-describe('CreateTournament', () => {
-  let component: CreateTournament;
 
-  beforeEach(() => {
-    component = new CreateTournament();
-    document.body.innerHTML = `
-      <input id="nome" />
-      <input id="jogo" />
-      <input id="data" />
-      <input id="endereco" />
-      <input id="minimo" />
-      <input id="maximo" />
-      <input id="dataLimite" />
-      <input id="valorIngresso" />
-      <input id="premiacao" />
-    `;
+describe('TournamentList', () => {
+  let component: TournamentList;
+  let fixture: ComponentFixture<TournamentList>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TournamentList]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TournamentList);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('deve exibir alerta se campos obrigatórios não forem preenchidos', () => {
-    spyOn(window, 'alert');
-    component.criarTorneio();
-    expect(window.alert).toHaveBeenCalledWith('Preencha todos os campos obrigatórios!');
-  });
-
-  it('deve criar o torneio com sucesso quando os campos obrigatórios forem preenchidos', () => {
-    spyOn(window, 'alert');
-    (document.getElementById('nome') as HTMLInputElement).value = 'Torneio CS';
-    (document.getElementById('jogo') as HTMLInputElement).value = 'CS2';
-    (document.getElementById('data') as HTMLInputElement).value = '2025-10-21T10:00';
-
-    component.criarTorneio();
-
-    expect(window.alert).toHaveBeenCalledWith('Torneio criado com sucesso!');
-  });
-
-  it('deve limpar todos os campos ao chamar limparCampos()', () => {
-    (document.getElementById('nome') as HTMLInputElement).value = 'Teste';
-    (document.getElementById('jogo') as HTMLInputElement).value = 'CS2';
-
-    component.limparCampos();
-
-    const nome = (document.getElementById('nome') as HTMLInputElement).value;
-    const jogo = (document.getElementById('jogo') as HTMLInputElement).value;
-
-    expect(nome).toBe('');
-    expect(jogo).toBe('');
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
--- src/frontend/glitch_frontend/src/app/pages/create-tournament/create-tournament.ts
+++ src/frontend/glitch_frontend/src/app/pages/create-tournament/create-tournament.ts
@@ -1,51 +1,111 @@
 import { Component } from '@angular/core';
 import { Navigation } from "../../components/navigation/navigation";
 import { ButtonComponent } from "../../components/button/button";
-import { Footer } from '../../components/footer/footer';
-import { ThemeToggler } from '../../components/theme-toggler/theme-toggler';
+import { InputComponent } from "../../components/input/input";
+import { FormControl, FormGroup, Validators, ReactiveFormsModule, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
+import { LucideAngularModule } from 'lucide-angular';
+import { CompetitorLevelComponent } from "../../components/toggle-group/toggle-group";
+import { ThemeToggler } from "../../components/theme-toggler/theme-toggler";
+import { ToggleButtonComponent } from "../../components/toggle-button/toggle.button"
 
 @Component({
   selector: 'app-create-tournament',
-  imports: [Navigation, ButtonComponent, Footer, ThemeToggler],
+  standalone: true,
+  imports: [
+  ReactiveFormsModule,
+  LucideAngularModule ,
+  InputComponent,
+  CompetitorLevelComponent,
+  ToggleButtonComponent,
+  ButtonComponent,
+  Navigation
+],
   templateUrl: './create-tournament.html',
   styleUrls: ['./create-tournament.scss']
 })
 export class CreateTournament {
-  criarTorneio() {
-    const nome = (document.getElementById('nome') as HTMLInputElement).value;
-    const jogo = (document.getElementById('jogo') as HTMLInputElement).value;
-    const data = (document.getElementById('data') as HTMLInputElement).value;
-    const endereco = (document.getElementById('endereco') as HTMLInputElement).value;
-    const minimo = (document.getElementById('minimo') as HTMLInputElement).value;
-    const maximo = (document.getElementById('maximo') as HTMLInputElement).value;
-    const dataLimite = (document.getElementById('dataLimite') as HTMLInputElement).value;
-    const valorIngresso = (document.getElementById('valorIngresso') as HTMLInputElement).value;
-    const premiacao = (document.getElementById('premiacao') as HTMLInputElement).value;
-
-    if (!nome || !jogo || !data) {
-      alert('Preencha todos os campos obrigatórios!');
-      return;
-    }
-
-    const torneio = {
-      nome,
-      jogo,
-      data,
-      endereco,
-      minimo,
-      maximo,
-      dataLimite,
-      valorIngresso,
-      premiacao
-    };
-
-    console.log('Torneio criado:', torneio);
-    alert('Torneio criado com sucesso!');
-    this.limparCampos();
+submit() {
+  if (this.form.invalid) {
+    this.form.markAllAsTouched();
+  // Debug
+  Object.keys(this.form.controls).forEach(key => {
+const control = this.form.get(key);
+  if (control?.invalid) {
+  console.log(`Campo inválido: ${key}`, control.errors);
   }
-
-  limparCampos() {
-    const inputs = document.querySelectorAll('input');
-    inputs.forEach(input => (input.value = ''));
+  });
+  return;
   }
+  console.log('Torneio criado com sucesso', this.form.value);
+}
+
+dateNotPastValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (!control.value) return null;
+    const today = new Date();
+    const todayStr = today.toISOString().split('T')[0];
+    const selectedDateStr = control.value;
+    // Se a data selecionada for menor que hoje, retorna erro
+    return selectedDateStr < todayStr ? { pastDate: true } : null;
+  };
+}
+
+ form = new FormGroup({
+  tournamentName: new FormControl('', [
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(50)
+  ]),
+  gameName: new FormControl('', [
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(30)
+  ]),
+  tournamentDate: new FormControl('', [
+    Validators.required,
+    this.dateNotPastValidator()
+  ]),
+  competitorLevel: new FormControl('', [Validators.required]),
+  typeCompetition: new FormControl('', [Validators.required]),
+  typeRanking: new FormControl('', [Validators.required]),
+  typePlaceTournament: new FormControl('', [Validators.required]),
+  addressTournament: new FormControl(''),
+  addressNumberTournament: new FormControl(''),
+  neighborhoodTournament: new FormControl(''),
+  cityTournament: new FormControl(''),
+  stateTournament: new FormControl(''),
+  cepTournament: new FormControl(''),
+  minParticipants: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)]),
+  maxParticipants: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)]),
+  registrationsDate: new FormControl('', [Validators.required, this.dateNotPastValidator()]),
+  registrationAvailable: new FormControl(false),
+  typeGroup: new FormControl('', [Validators.required]),
+  quantityGroups: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)]),
+  randomizeGroups: new FormControl(false),
+  entryOnlyGroups: new FormControl(false),
+  ticketTournament: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)])
+});
+
+  get tournamentNameControl() { return this.form.get('tournamentName') as FormControl; }
+  get gameNameControl() { return this.form.get('gameName') as FormControl; }
+  get tournamentDateControl() { return this.form.get('tournamentDate') as FormControl; }
+  get competitorLevelControl() { return this.form.get('competitorLevel') as FormControl; }
+  get typeCompetitionControl() { return this.form.get('typeCompetition') as FormControl; }
+  get typeRankingLevelControl() { return this.form.get('typeRanking') as FormControl; }
+  get typePlaceTournamentControl() { return this.form.get('typePlaceTournament') as FormControl; }
+  get addressTournamentControl() { return this.form.get('addressTournament') as FormControl; }
+  get addressNumberTournamentControl() { return this.form.get('addressNumberTournament') as FormControl; }
+  get neighborhoodTournamentControl() { return this.form.get('neighborhoodTournament') as FormControl; }
+  get cityTournamentControl() { return this.form.get('cityTournament') as FormControl; }
+  get stateTournamentControl() { return this.form.get('stateTournament') as FormControl; }
+  get cepTournamentControl() { return this.form.get('cepTournament') as FormControl; }
+  get minParticipantsControl() { return this.form.get('minParticipants') as FormControl; }
+  get maxParticipantsControl() { return this.form.get('maxParticipants') as FormControl; }
+  get registrationsDateControl() { return this.form.get('registrationsDate') as FormControl; }
+  get registrationAvailableControl() { return this.form.get('registrationAvailable') as FormControl; }
+  get typeGroupControl () { return this.form.get('typeGroup') as FormControl; }
+  get quantityGroupsControl() { return this.form.get('quantityGroups') as FormControl; }
+  get randomizeGroupsControl() { return this.form.get('randomizeGroups') as FormControl; }
+  get entryOnlyGroupsControl() { return this.form.get('entryOnlyGroups') as FormControl; }
+  get ticketTournamentControl() { return this.form.get('ticketTournament') as FormControl; }
 }
