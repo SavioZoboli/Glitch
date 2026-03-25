11cc9ed1bc86a66855680df952221ab4aa613141
Leticia Zalasik <leticiazalasik@gmail.com>
2026-03-25 09:25:50 -0300

    Nome do jogador trazido do localstorage; melhoias de layout; uso de dados mock para teste e cri\ção da tabela de relatório de torneios

--- src/frontend/glitch_frontend/src/app/pages/dashboard/dashboard.ts
+++ src/frontend/glitch_frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,50 +1,90 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { Footer } from '../../components/footer/footer';
 import { MatIconModule } from '@angular/material/icon';
-import { Navigation } from "../../components/navigation/navigation";
-import { TeamInviteBoxComponent } from "../../components/team-invite-box-component/team-invite-box-component";
+import { Navigation } from '../../components/navigation/navigation';
+import { TeamInviteBoxComponent } from '../../components/team-invite-box-component/team-invite-box-component';
 import { TournamentService } from '../../services/tournament-service';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
-    selector: 'app-dashboard',
-    standalone: true,
-    templateUrl: './dashboard.html',
-    styleUrls: ['./dashboard.scss'],
-    encapsulation: ViewEncapsulation.None,
-    imports: [
-    MatIconModule,
-    Navigation,
-    TeamInviteBoxComponent,
-    AsyncPipe
-]
+  selector: 'app-dashboard',
+  standalone: true,
+  templateUrl: './dashboard.html',
+  styleUrls: ['./dashboard.scss'],
+  encapsulation: ViewEncapsulation.None,
+  imports: [MatIconModule, Navigation, TeamInviteBoxComponent, AsyncPipe],
 })
 export class DashboardComponent {
-    playerName = 'Jogador';
-    private torneiosInscritosSubject:BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
-    torneiosInscritos:Observable<any[]> = this.torneiosInscritosSubject.asObservable();
+  playerName = 'Jogador';
+  private torneiosInscritosSubject: BehaviorSubject<any[]> =
+    new BehaviorSubject<any[]>([]);
+  torneiosInscritos: Observable<any[]> =
+    this.torneiosInscritosSubject.asObservable();
 
-constructor(
-    private torneioService:TournamentService
+  private relatoriosSubject = new BehaviorSubject<any[]>([]);
+  relatorios = this.relatoriosSubject.asObservable();
+  constructor(private torneioService: TournamentService) {
+    this.buscarRelatorioDeTorneios();
+  }
 
-){
-    this.buscarRelatorioDeTorneios()
-}
+  //Trazer o nome do localstorage
+  nickname: string = '';
 
+  ngOnInit() {
+    const userData = localStorage.getItem('userData');
 
-buscarRelatorioDeTorneios(){
+    if (userData) {
+      const parsed = JSON.parse(userData);
+      this.nickname = parsed.nickname;
+    }
+  }
+
+  buscarRelatorioDeTorneios() {
     this.torneioService.buscarTorneiosDoUsuario().subscribe({
-        next:(res)=>{
-            res.forEach((t:any)=>{
-                t.data_realizacao = new Date(t.data_realizacao)
-            })
-            this.torneiosInscritosSubject.next(res)
-        },
-        error:(err)=>{
-            console.log(err)
-        }
-    })
-}
+      next: (res) => {
+        res.forEach((t: any) => {
+          t.data_realizacao = new Date(t.data_realizacao);
+        });
+        this.torneiosInscritosSubject.next(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 
+  torneiosMock = [
+    {
+      id_torneio: '1',
+      data_realizacao: new Date(),
+      nome_torneio: 'Campeonato Teste',
+      nome_jogo: 'FIFA 24',
+      organizador: 'Letícia',
+      status_inscricao: 'INSCRITO',
+    },
+    {
+      id_torneio: '2',
+      data_realizacao: new Date(),
+      nome_torneio: 'Copa Debug',
+      nome_jogo: 'CS2',
+      organizador: 'Admin',
+      status_inscricao: 'INSCRITO',
+    },
+  ];
+
+  relatoriosMock = [
+    {
+      id: '1',
+      data: new Date(),
+      nome: 'Campeonato X',
+      situacao: 'FINALIZADO',
+    },
+    {
+      id: '2',
+      data: new Date(),
+      nome: 'Copa Y',
+      situacao: 'FINALIZADO',
+    },
+  ];
 }
