4b8b3a6fe97ed0939d432a5bed76fbb39109da31
Leticia Zalasik <leticiazalasik@gmail.com>
2026-04-01 16:40:59 -0300

    Modal para convidar paticipantes a equipe e outras mudanças de layout; retornar a tela de equipes depois que a equipe foi criada; dar feedback caso a pessoa não preencha o nome da equipe

--- src/frontend/glitch_frontend/src/app/pages/create-group/create-group.ts
+++ src/frontend/glitch_frontend/src/app/pages/create-group/create-group.ts
@@ -1,113 +1,134 @@
 import { Component, OnInit } from '@angular/core';
-import { Navigation } from "../../components/navigation/navigation";
-import { InputComponent } from "../../components/input/input";
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ButtonComponent } from "../../components/button/button";
+import { Navigation } from '../../components/navigation/navigation';
+import { InputComponent } from '../../components/input/input';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonComponent } from '../../components/button/button';
 import { EquipeService } from '../../services/equipe-service';
 import { SystemNotificationService } from '../../services/misc/system-notification-service';
-import { Usuario, UsuarioResumo, UsuarioService } from '../../services/usuario-service';
+import {
+  Usuario,
+  UsuarioResumo,
+  UsuarioService,
+} from '../../services/usuario-service';
 import { catchError, EMPTY, Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-group',
-  imports: [Navigation, InputComponent, ButtonComponent, AsyncPipe, ReactiveFormsModule],
+  imports: [
+    Navigation,
+    InputComponent,
+    ButtonComponent,
+    AsyncPipe,
+    ReactiveFormsModule,
+  ],
   templateUrl: './create-group.html',
-  styleUrl: './create-group.scss'
+  styleUrl: './create-group.scss',
 })
 export class CreateGroup implements OnInit {
-
   jogadores$!: Observable<UsuarioResumo[]>;
 
-  convidados: string[] = []
+  convidados: string[] = [];
 
   form: FormGroup = new FormGroup({
-    nome: new FormControl('', Validators.required)
-  })
+    nome: new FormControl('', Validators.required),
+  });
 
-
-  get nomeControl(): FormControl { return this.form.get('nome') as FormControl }
+  get nomeControl(): FormControl {
+    return this.form.get('nome') as FormControl;
+  }
 
   constructor(
     private equipeService: EquipeService,
     private notifService: SystemNotificationService,
     private usuarioService: UsuarioService,
-    private router:Router
-  ) {
-
-  }
+    private router: Router,
+  ) {}
 
   submit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
     if (this.form.valid) {
       let nome = this.nomeControl.value;
       this.equipeService.addEquipe(nome).subscribe({
         next: (res) => {
-          console.log(res)
-          this.notifService.notificar('sucesso', 'Equipe criada com sucesso')
+          console.log(res);
+          this.notifService.notificar('sucesso', 'Equipe criada com sucesso');
           if (this.convidados.length > 0 && res.equipe) {
-            this.notifService.notificar('info', 'Convidando jogadores...')
+            this.notifService.notificar('info', 'Convidando jogadores...');
             let invites = this.convidados;
             invites.forEach((n: string) => {
-              this.convidarJogador(res.equipe, n)
-            })
-            this.router.navigate(['/groups'])
+              this.convidarJogador(res.equipe, n);
+            });
           }
+
+          this.router.navigate(['/groups']);
         },
         error: (err) => {
-          console.log(err)
-          this.notifService.notificar('erro', 'Erro ao criar equipe')
-        }
-      })
+          console.log(err);
+          this.notifService.notificar('erro', 'Erro ao criar equipe');
+        },
+      });
     }
   }
 
   private convidarJogador(equipe: string, jogador: string) {
     this.equipeService.convidarJogador(equipe, jogador).subscribe({
       next: (res) => {
-        this.notifService.notificar('sucesso', `Jogador ${jogador} convidado`)
+        this.notifService.notificar('sucesso', `Jogador ${jogador} convidado`);
       },
       error: (err) => {
-        console.log(err)
-        this.notifService.notificar('erro', `Erro ao convidar ${jogador}`)
-      }
-    })
+        console.log(err);
+        this.notifService.notificar('erro', `Erro ao convidar ${jogador}`);
+      },
+    });
   }
 
   toggleConvidado(nickname: string) {
-    let index = this.convidados.indexOf(nickname)
+    let index = this.convidados.indexOf(nickname);
     if (index != -1) {
-      this.convidados.splice(index, index)
+      this.convidados.splice(index, 1);
     } else {
-      this.convidados.push(nickname)
+      this.convidados.push(nickname);
     }
+    console.log('Selecionados:', this.convidados);
+  }
+
+  isSelecionado(nickname: string): boolean {
+    return this.convidados.includes(nickname);
   }
 
   clearForm() {
-    this.nomeControl.reset()
-    this.convidados.forEach(c => {
-      let input = document.getElementById('checkbox' + c) as HTMLInputElement
+    this.nomeControl.reset();
+    this.convidados.forEach((c) => {
+      let input = document.getElementById('checkbox' + c) as HTMLInputElement;
       if (input) {
         input.checked = false;
       }
-    })
-    this.convidados = []
+    });
+    this.convidados = [];
   }
-
 
   ngOnInit(): void {
     this.jogadores$ = this.usuarioService.getUsuariosResumido().pipe(
-      catchError(err => {
+      catchError((err) => {
         // Se ocorrer um erro na API, podemos logar e retornar um array vazio.
         // Isso evita que a tela quebre se a chamada falhar.
         console.error('Erro ao buscar jogadores:', err);
         return EMPTY; // ou return of([]); se preferir emitir um array vazio.
-      })
+      }),
     );
   }
 
-  return(){
-    this.router.navigate(['/groups'])
+  return() {
+    this.router.navigate(['/groups']);
   }
-
 }
