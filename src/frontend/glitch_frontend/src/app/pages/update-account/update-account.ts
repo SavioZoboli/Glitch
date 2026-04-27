f5afe41529a88c2de45e2cd37ea1ef42c58d010c
Leticia Zalasik <leticiazalasik@gmail.com>
2026-04-27 16:51:44 -0300

    Adição do campo sobre mim e resposividade para página de editar perfil

--- src/frontend/glitch_frontend/src/app/pages/update-account/update-account.ts
+++ src/frontend/glitch_frontend/src/app/pages/update-account/update-account.ts
@@ -1,10 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Navigation } from "../../components/navigation/navigation";
-import { InputComponent } from "../../components/input/input";
-import { ButtonComponent } from "../../components/button/button";
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatIcon } from "@angular/material/icon";
-import { Router, RouterLink } from '@angular/router';
+import { Navigation } from '../../components/navigation/navigation';
+import { InputComponent } from '../../components/input/input';
+import { ButtonComponent } from '../../components/button/button';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import { Usuario, UsuarioService } from '../../services/usuario-service';
 import { Subscription } from 'rxjs';
 import { SystemNotificationService } from '../../services/misc/system-notification-service';
@@ -13,47 +17,51 @@ import { SystemNotificationService } from '../../services/misc/system-notificati
   selector: 'app-update-account',
   imports: [Navigation, InputComponent, ButtonComponent, ReactiveFormsModule],
   templateUrl: './update-account.html',
-  styleUrl: './update-account.scss'
+  styleUrl: './update-account.scss',
 })
 export class UpdateAccount implements OnInit, OnDestroy {
-
   getDadosSubscription: Subscription | undefined;
-  updateSubscription:Subscription|undefined;
+  updateSubscription: Subscription | undefined;
 
-  dadosUsuario: Usuario | undefined
+  dadosUsuario: Usuario | undefined;
 
-  constructor(private usuarioService: UsuarioService,private sysNotifService:SystemNotificationService,private router:Router) {
-
-  }
+  constructor(
+    private usuarioService: UsuarioService,
+    private sysNotifService: SystemNotificationService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
     this.getDadosSubscription = this.usuarioService.getDadosUpdate().subscribe({
       next: (res) => {
         if (res) {
-          console.log(res)
+          console.log(res);
           if (!res.id && !res.pessoa.id) {
-            this.sysNotifService.notificar('erro','Não foi possível buscar os dados')
-            console.log(res)
+            this.sysNotifService.notificar(
+              'erro',
+              'Não foi possível buscar os dados',
+            );
+            console.log(res);
           } else {
             this.dadosUsuario = this.validaResposta(res);
-            this.initFormulario()
+            this.initFormulario();
           }
-
         }
       },
       error: (error) => {
-        console.log(error)
-      }
-    })
+        console.log(error);
+      },
+    });
   }
 
   private validaResposta(res: any): Usuario {
-
     let dados: Usuario = {
       id: res.id,
       nickname: res.nickname,
       dt_criacao: new Date(res.dt_criacao),
-      ultima_altera_senha: res.ultima_altera_senha?new Date(res.ultima_altera_senha):null,
+      ultima_altera_senha: res.ultima_altera_senha
+        ? new Date(res.ultima_altera_senha)
+        : null,
       pessoa: {
         id: res.pessoa.id,
         nome: res.pessoa.nome,
@@ -63,57 +71,70 @@ export class UpdateAccount implements OnInit, OnDestroy {
         email: res.pessoa.email,
         telefone: res.pessoa.telefone,
         is_ativo: res.pessoa.is_ativo,
-        nacionalidade: res.pessoa.nacionalidade
-      }
-    }
+        nacionalidade: res.pessoa.nacionalidade,
+      },
+    };
     return dados;
   }
 
   private mascaraCPF(cpf: string): string {
-    return cpf.substring(8, 11).padStart(11, '#')
+    return cpf.substring(8, 11).padStart(11, '#');
   }
 
   form = new FormGroup({
-    email: new FormControl('', [
-      Validators.required,
-      Validators.email
-    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     phone: new FormControl('', [
       Validators.required,
-      Validators.pattern(/^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/) // (99) 99999-9999 ou 9999-9999
+      Validators.pattern(/^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/), // (99) 99999-9999 ou 9999-9999
     ]),
     nickname: new FormControl('', []),
-    nacionalidade: new FormControl('', [Validators.required])
+    nacionalidade: new FormControl('', [Validators.required]),
+    aboutMe: new FormControl('', [Validators.maxLength(250)]),
   });
 
-
   //Getters para usar no template
-  get emailControl() { return this.form.get('email') as FormControl; }
-  get phoneControl() { return this.form.get('phone') as FormControl; }
-  get nicknameControl() { return this.form.get('nickname') as FormControl; }
-  get nacionalidadeControl() { return this.form.get('nacionalidade') as FormControl; }
-
+  get emailControl() {
+    return this.form.get('email') as FormControl;
+  }
+  get phoneControl() {
+    return this.form.get('phone') as FormControl;
+  }
+  get nicknameControl() {
+    return this.form.get('nickname') as FormControl;
+  }
+  get nacionalidadeControl() {
+    return this.form.get('nacionalidade') as FormControl;
+  }
+  get aboutMeControl() {
+    return this.form.get('aboutMe') as FormControl;
+  }
   // Método de submit
   submit() {
     if (this.form.valid) {
       let dadosUpdate = {
-        id:this.dadosUsuario?.id,
-        email:this.emailControl.value,
-        telefone:this.phoneControl.value,
-        nickname:this.nicknameControl.value,
-        nacionalidade:this.nacionalidadeControl.value,
-      }
-      this.updateSubscription = this.usuarioService.updateUsuario(dadosUpdate).subscribe({
-        next:(res)=>{
-          console.log(res)
-          this.sysNotifService.notificar('sucesso','Atualizado com sucesso!')
-          this.router.navigate(['/profile'])
-        },
-        error:(error)=>{
-          console.log(error)
-          this.sysNotifService.notificar('erro','Erro ao salvar')
-        }
-      })
+        id: this.dadosUsuario?.id,
+        email: this.emailControl.value,
+        telefone: this.phoneControl.value,
+        nickname: this.nicknameControl.value,
+        nacionalidade: this.nacionalidadeControl.value,
+        aboutMe: this.aboutMeControl.value,
+      };
+      this.updateSubscription = this.usuarioService
+        .updateUsuario(dadosUpdate)
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this.sysNotifService.notificar(
+              'sucesso',
+              'Atualizado com sucesso!',
+            );
+            this.router.navigate(['/profile']);
+          },
+          error: (error) => {
+            console.log(error);
+            this.sysNotifService.notificar('erro', 'Erro ao salvar');
+          },
+        });
       // lógica de criação de conta aqui
     } else {
       console.log('Formulário inválido');
@@ -122,33 +143,33 @@ export class UpdateAccount implements OnInit, OnDestroy {
   }
 
   initFormulario() {
-    this.form.reset()
+    this.form.reset();
     if (!this.dadosUsuario || !this.dadosUsuario.pessoa) {
       return;
     }
     this.emailControl.setValue(this.dadosUsuario.pessoa.email);
-    this.phoneControl.setValue(this.dadosUsuario.pessoa.telefone)
-    this.nicknameControl.setValue(this.dadosUsuario.nickname)
-    this.nacionalidadeControl.setValue(this.dadosUsuario.pessoa.nacionalidade)
+    this.phoneControl.setValue(this.dadosUsuario.pessoa.telefone);
+    this.nicknameControl.setValue(this.dadosUsuario.nickname);
+    this.nacionalidadeControl.setValue(this.dadosUsuario.pessoa.nacionalidade);
   }
 
   ngOnDestroy(): void {
-    if(this.getDadosSubscription){
-      this.getDadosSubscription.unsubscribe()
+    if (this.getDadosSubscription) {
+      this.getDadosSubscription.unsubscribe();
     }
-    if(this.updateSubscription){
-      this.updateSubscription.unsubscribe()
+    if (this.updateSubscription) {
+      this.updateSubscription.unsubscribe();
     }
   }
 
   normalizeEmail(event: any) {
-  const value = event.target.value.replace(/[^a-zA-Z0-9@._-]/g, '');
-  event.target.value = value.toLowerCase();
-  this.emailControl.setValue(value.toLowerCase());
-}
+    const value = event.target.value.replace(/[^a-zA-Z0-9@._-]/g, '');
+    event.target.value = value.toLowerCase();
+    this.emailControl.setValue(value.toLowerCase());
+  }
 
   cancel() {
-  this.form.reset();
-  this.router.navigate(['/profile']);
-}
+    this.form.reset();
+    this.router.navigate(['/profile']);
+  }
 }
