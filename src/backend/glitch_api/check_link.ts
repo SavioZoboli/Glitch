18f230355d2b24db492dbdb3cabb16dad1c47b33
Gustavo Santana Jacinto <82461100+GustavoAPI@users.noreply.github.com>
2026-05-24 21:38:00 -0300

    chore: remove arquivos temporários de teste

--- src/backend/glitch_api/check_link.ts
+++
@@ -1,4 +0,0 @@
-﻿const { sequelize } = require('./src/config/database.config');
-sequelize.query("SELECT nome, link_transmissao FROM torneios")
-  .then(r => { console.log(JSON.stringify(r[0], null, 2)); process.exit(0); })
-  .catch(e => { console.log('ERRO:', e.message); process.exit(1); });
--- src/backend/glitch_api/teste_update.ts
+++
@@ -1,4 +0,0 @@
-﻿const { sequelize } = require('./src/config/database.config');
-sequelize.query("UPDATE torneios SET link_transmissao = 'https://kick.com/teste' WHERE nome = 'teste'")
-  .then(r => { console.log('OK'); process.exit(0); })
-  .catch(e => { console.log('ERRO:', e.message); process.exit(1); });
